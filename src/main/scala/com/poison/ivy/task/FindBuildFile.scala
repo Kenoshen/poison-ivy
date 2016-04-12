842959c9be9d13953a204ec82a5cfd3fca1c0901
Michael Wingfield <michael.wingfield@twcable.com>
2016-04-12 00:31:35 -0600

    added the sample build file

--- src/main/scala/com/poison/ivy/task/FindBuildFile.scala
+++ src/main/scala/com/poison/ivy/task/FindBuildFile.scala
@@ -8,11 +8,11 @@ object FindBuildFile extends Task[Unit, File] {
   override lazy val description: String = "Try to find the build file for parsing"
 
   override def run(input: Unit): Future[File] = {
-    val file = new File("poison.build")
+    val file = new File("poison.yml")
     if (file.exists)
       if (!file.isDirectory)
         Future.successful(file)
-      else throw new TaskException(s"The posion.build is marked as a directory, please make it a file")
-    else throw new TaskException(s"Could not find poison.build file at ${file.getAbsolutePath}")
+      else throw new TaskException(s"The posion.yml is marked as a directory, please make it a file")
+    else throw new TaskException(s"Could not find poison.yml file at ${file.getAbsolutePath}")
   }
 }
