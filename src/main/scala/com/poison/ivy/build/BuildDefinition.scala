e5437267f8e7337e188182b74a4397a7e6667af0
Michael Wingfield <michael.wingfield@twcable.com>
2016-04-13 15:42:48 -0600

    finished testing build classes

--- src/main/scala/com/poison/ivy/build/BuildDefinition.scala
+++ src/main/scala/com/poison/ivy/build/BuildDefinition.scala
@@ -1,15 +1,29 @@
 package com.poison.ivy.build
 
-import net.jcazevedo.moultingyaml.{YamlString, YamlObject}
+import com.poison.ivy.task.TaskException
+import net.jcazevedo.moultingyaml.{YamlArray, YamlString, YamlObject}
 
-case class BuildDefinition(libraries: BuildLibraries, variables: BuildVariables, defaults: BuildDefaults, modules: BuildModules) {
-
+case class BuildDefinition(variables: BuildVariables, libraries: BuildLibraries, defaults: BuildDefaults, modules: BuildModules) {
+  def scrub:BuildDefinition = {
+    val scrubbedVariables = variables.scrub
+    val scrubbedLibraries = libraries.scrub(scrubbedVariables)
+    val scrubbedDefaults = defaults.scrub(scrubbedVariables, scrubbedLibraries)
+    val scrubbedModules = modules.scrub(scrubbedVariables, scrubbedDefaults, scrubbedLibraries)
+    BuildDefinition(scrubbedVariables, scrubbedLibraries, scrubbedDefaults, scrubbedModules)
+  }
 }
 
 
 object BuildDefinition {
   def apply(yamlObj: YamlObject): BuildDefinition = {
-    ???
-
+    val obj = yamlObj.fields
+    val variables:BuildVariables = obj.get(YamlString("variables")).map(y => BuildVariables(y.asYamlObject)).getOrElse(BuildVariables())
+    val libraries:BuildLibraries = obj.get(YamlString("libraries")).map(y => BuildLibraries(y.asYamlObject, variables)).getOrElse(BuildLibraries())
+    val defaults:BuildDefaults = obj.get(YamlString("defaults")).map(y => BuildDefaults(y.asYamlObject, variables, libraries)).getOrElse(BuildDefaults())
+    val modules:BuildModules = obj.get(YamlString("modules")).map {
+      case y:YamlArray => BuildModules(y, variables, defaults, libraries)
+      case x => throw new TaskException("'modules' must be an array of modules")
+    }.getOrElse(BuildModules())
+    BuildDefinition(variables, libraries, defaults, modules)
   }
 }
---
+++ src/test/scala/com/poison/ivy/build/BuildDefinitionTest.scala
@@ -0,0 +1,51 @@
+package com.poison.ivy.build
+
+import com.poison.ivy.task.TaskException
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class BuildDefinitionTest extends FunSuite {
+  test("validate basic build definition"){
+    val definition = BuildDefinition(
+      BuildVariables(BuildVariable("version", "1.2.3"), BuildVariable("SECOND_PATH", "foobar$version")),
+      BuildLibraries(BuildLibrary("library", "yay"), BuildLibrary("lib", Seq("library"))),
+      BuildDefaults(group = Option("com.group"), description = Option("hello world"), dependencies = Seq("lib")),
+      BuildModules(
+        BuildModule(
+          "first",
+          "first.group",
+          "$version",
+          None,
+          None,
+          Seq("library"),
+          "2.10",
+          None
+        ),
+        BuildModule(
+          "second",
+          "second.group",
+          "$version",
+          Option("$SECOND_PATH"),
+          Option("goodbye"),
+          Nil,
+          "2.11",
+          Option(BuildPublish("localhost:9200", "mvn", None))
+        )
+      )
+    ).scrub
+
+    assert(definition.modules.modules.head.version == "1.2.3")
+    assert(definition.modules.modules.head.description.get == "hello world")
+    assert(definition.modules.modules.head.dependencies.head == "yay")
+    assert(definition.modules.modules.head.dependencies.length == 1)
+
+    assert(definition.modules.modules.last.version == "1.2.3")
+    assert(definition.modules.modules.last.path.get == "foobar1.2.3")
+    assert(definition.modules.modules.last.description.get == "goodbye")
+    assert(definition.modules.modules.last.dependencies.head == "yay")
+    assert(definition.modules.modules.last.dependencies.length == 1)
+    assert(definition.modules.modules.last.publish.get.url == "localhost:9200")
+  }
+}
